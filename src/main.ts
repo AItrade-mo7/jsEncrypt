ce6588ef329ef7159042aa051b04b99a2e914ef6
墨七 <mo7@mo7.cc>
2023-04-14 15:38:48 +0800

    加密

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { Md5, Encrypt } from './Encrypt';
-import { AESEncrypt, AESDeCode } from './AES';
+import { AseEncrypt } from './AES';
 window.mo7Encrypt = Encrypt;
 window.mo7Md5 = Md5;
-window.mo7AESEncrypt = AESEncrypt;
-window.mo7AESDeCode = AESDeCode;
+window.mo7AseEncrypt = AseEncrypt;
 
 const SecretKey = window.mo7Md5('AItrade.net from mo7cc');
 window.mo7SecretKey = SecretKey;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,7 +6,6 @@ declare global {
     mo7Encrypt: (msg: string) => string;
     mo7Md5: (msg: string) => string;
     mo7SecretKey: string;
-    mo7AESEncrypt: (data: string, Key: string) => string;
-    mo7AESDeCode: (data: string, Key: string) => string;
+    mo7AseEncrypt: (data: string) => string;
   }
 }
